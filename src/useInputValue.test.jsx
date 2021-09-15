a24d06ade29cc8d3cdc252f5f8838ca4d777acaf
enjinerd <dev@ron.my.id>
2021-09-15 16:34:08 +0700

    fix(useInputValue): fix testing logic on useInputValue

--- src/useInputValue.test.jsx
+++ src/useInputValue.test.jsx
@@ -1,9 +1,10 @@
-import { renderHook } from '@testing-library/react-hooks';
-import useInputValue from './useInputValue'
+import { renderHook, act } from '@testing-library/react-hooks';
+import {useInputValue} from './useInputValue'
 
 describe('Testing useInputValue', () => {
   it('should have value', () => {
-    const {result, rerender} = renderHook(() => useInputValue(12));
-    console.log(result.current.value) 
+    const {result, rerender} = renderHook(() => useInputValue('ROni'))
+    act(() => result.current.onChange({target: {value: 'succes'}}))
+    expect(result.current.value).toEqual('succes')
   });
 })
